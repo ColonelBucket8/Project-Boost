b9826407e7614f27b3356f6290d2f5553fd7179c
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-12-24 19:16:51 +0800

    Move obstacle over period of time

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,18 +4,32 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField][Range(0, 1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 4f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         startingPosition = transform.position;
-        Debug.Log(startingPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Continually growing overt ime
+        float cyles = Time.time / period;
+
+        // Constant value of 6.283
+        const float tau = Mathf.PI * 2;
+        // Going from -1 to 1
+        float rawSinWave = Mathf.Sin(tau * cyles);
+        // float rawSinWave = (rawSineWave + 1f) / 2f;
+
+        // Recalculated to go from 0 to 1 so its cleaner
+        movementFactor = Mathf.Abs(rawSinWave);
+
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
