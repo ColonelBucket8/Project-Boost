bb9e3a95ff67bc4593d90747179fd5cabf282b3e
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-12-24 19:01:05 +0800

    Add oscillator script to object for movement

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField][Range(0, 1)] float movementFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
